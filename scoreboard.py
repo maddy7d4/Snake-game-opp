f2994442b84262e6c0fb1a5917d5cb47bbdd5e24
M A D D Y <102873005+github-maddy@users.noreply.github.com>
2022-08-04 08:51:40 +0530

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,3 +1,4 @@
+from asyncore import write
 from turtle import Turtle
 
 class Scoreboard(Turtle):
@@ -5,13 +6,28 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.points = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         self.score()
     def score(self):
         self.hideturtle()
         self.penup()
         self.color("white")
         self.goto(x=0,y=250)
-        self.write(f'Score : {self.points}',move = False,align = 'center',font=('courier',20,'normal'))
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f'Score : {self.points} High Score : {self.highscore}',move = False,align = 'center',font=('courier',20,'normal'))
+
+
+    def reset(self):
+        if self.points>self.highscore:
+            self.highscore = self.points
+            with open("data.txt",mode="w") as data:
+                data.write(f"{self.highscore}")
+        self.points = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.points += 1
